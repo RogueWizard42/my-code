2d95968ebe5b0be1077c8e3909375d0d634375fd
Matt <152105280+RogueWizard42@users.noreply.github.com>
2025-01-28 18:53:44 -0500

    Add files via upload
    My first upload...neat

---
+++ Getting Started with Python/Chapter 2: Variables and Expressions/Assignment2_3.py
@@ -0,0 +1,13 @@
+#Basic pay calculation
+
+hours = input('Enter Hours: ')
+rate = input('Enter Rate: ')
+totalpay = float(rate) * float(hours)
+print('Pay:',totalpay)
+
+
+                     
+
+
+    
+    
