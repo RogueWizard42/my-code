cad4c932b5f01748a57ae011da95914a62f60ff6
roguewizard42 <roguewizard42@gmail.com>
2025-03-10 17:06:28 -0400

    Initial creation of dragon_realm.py

---
+++ dragon_realm.py
@@ -0,0 +1,8 @@
+# Dragon Realm game created by Al Sweigart - reworked by me
+#In this game, the player is in a land full of dragons. The dragons all live in caves with their large
+# piles of collected treasure. Some dragons are friendly and share their treasure with you. Other
+# dragons are hungry and eat anyone who enters their cave. The player is in front of two caves, one
+# with a friendly dragon and the other with a hungry dragon. The player must choose between the
+# two.
+
+
